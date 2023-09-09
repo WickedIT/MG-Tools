9ec13b03fddef07737e857b8c6bd19e66bf3296e
WickedIT <97221806+WickedIT@users.noreply.github.com>
2023-09-08 20:15:07 -0700

    Update Compare-Hashkey.psm1

--- Compare-Hashkey.psm1
+++ Compare-Hashkey.psm1
@@ -1,7 +1,8 @@
 function Compare-Hashkey {
     $download_hash = Read-Host "Please enter the downloads expected sha256."
     $exe_path = Read-Host "Please provide the EXE's download path."
-    $exe_hash = Get-FileHash -Path "$exe_path" -Algorithm SHA256
+    $find_exe_path = Get-ChildItem -Path "C:\Users\pcadmin\Downloads\" | where-object {$_.name -like "$exe_path"} | select-object -ExpandProperty PSChildName
+    $exe_hash = Get-FileHash -Path "C:\Users\pcadmin\Downloads\$find_exe_path" -Algorithm SHA256
     $result = Compare-Object -ReferenceObject "$download_hash" -DifferenceObject "$($exe_hash.Hash)"
     Write-Output $result
 
