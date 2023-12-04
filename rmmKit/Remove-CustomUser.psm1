960a649f51b419bd693a12917f36ecd1b1351154
WickedIT <97221806+WickedIT@users.noreply.github.com>
2023-12-04 01:30:32 -0800

    add to scripts
    changes and additions to some scripts

--- rmmKit/Remove-CustomUser.psm1
+++ rmmKit/Remove-CustomUser.psm1
@@ -24,3 +24,40 @@ Export-ModuleMember -Alias rclu
 Get-CimInstance -Class Win32_UserProfile | Where-Object { $_.Localpath.split('\')[-1] -eq '###' } | Remove-CimInstance
 #>
 
+function Remove-WinUser {
+    <#
+    .SYNOPSIS
+        Displays list of all user profiles and prompt for which profile to remove.
+    .DESCRIPTION
+        Pull's full list of User Profiles on the computer in a list, then waits for user to input which user they want to remove from the computer. Then confirms the selection.
+    #>
+        [CMDletBinding(SupportsShouldProcess)]
+        param($VerbosePerference = "Continue")
+        BEGIN{
+            $UserProfiles = Get-CimInstance -ClassName Win32_UserProfile
+        $UserProfileNames = foreach ($user in $userprofiles) {
+                                [PSCustomObject]@{Name = $user.LocalPath.split('\')[-1]}
+                            }
+        $UserProfileNames | Out-Host
+            
+        }
+        PROCESS{
+            $UserInput = Read-Host "Please Select a Profile to be removed, wildcard input not aloud."
+            $selection = $UserProfiles | Where-Object{$_.LocalPath -like "C:\Users\$UserInput"}
+            $selectedName = $selection.LocalPath.split('\')[-1]
+            Write-Verbose "Removing UserProfile $selectedName ..."
+        }
+        END{
+            if ($null -ne $selection) {
+                $selection | Remove-CimInstance -Confirm
+            }
+            else{
+                Write-Verbose "No match found. Please try again."
+            }
+        }
+    }
+    Remove-WinUser
+    
+    
+    
+    
---
+++ toolKit/Disable-USBPowerManagment.ps1
@@ -0,0 +1,28 @@
+﻿function Disable-USBPowerManagment {
+<#
+.SYNOPSIS
+	Sets all USB Hub power_management options to $false
+.DESCRIPTION
+	Uses Get-CimInstance to find all Devices with Power_Management settings; then disables it. This does not affect K/M to wake device (the PC is already incapable of turning those off but the option to wake remains). This function is minimally invasive because the classname is designed such that the only real change it is capable of making is that specific setting where it is applied.
+#>
+    BEGIN{#Grabs USB Hubs.
+        $USBPowerHubs = Get-CimInstance -Namespace root\wmi -ClassName MSPower_DeviceEnable
+    }
+    PROCESS{#Feeds each hub through Set-CimInstance to set the property to $false.
+        foreach ($usb in $USBPowerHubs) {
+            $usb | Set-CimInstance -Property @{Enable=$false}
+        }
+    }
+    END{
+    }
+}
+Disable-USBPowerManagment
+<#
+.SIGNATURE
+Michael Gagnon (IT Support Technician)
+
+.DATE
+January 17th, 2023
+
+#>
+
