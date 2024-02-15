9324ea3c9c1986696e478f72e352c2b9697497ab
WickedIT <michael.gagnonf@gmail.com>
2024-02-14 16:46:39 -0800

    Update BackupHealth.psm1

--- reportingKit/BackupHealth.psm1
+++ reportingKit/BackupHealth.psm1
@@ -1,5 +1,19 @@
 function Invoke-BackupHealth {
     <#
     use modules from window-backup-server to hopefully start and manage backups. need to look more into offering to decide how I will utilize this function.
+
+    create new policy and store in a variable
+
+    create filespec for c: in variable
+
+    apply filespec to policy_var
+
+    create backup target location in variable
+
+    apply backup location to policy
+
+    
     #> 
+    
+
     }
