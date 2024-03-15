29afd46ae10b9c5ca24a365fdd1f00dfab33ad35
WickedIT <michael.gagnonf@gmail.com>
2024-03-14 19:11:45 -0700

    docker-file u pdate
    updated syntax and added another compose entry for chess.

--- rmmKit/SessionManagement.psm1
+++ rmmKit/SessionManagement.psm1
@@ -1,5 +1,7 @@
 function Invoke-SessionManagement {
     <#
-    manage open sessions on devices and close them if they should not be there or if they have been open to long. mostly jsut check is and has historically been opened and (hopefully) what account was used to connect
+    manage open sessions on devices and close them if they should not be there or 
+    if they have been open to long. mostly just check is and has historically been
+    opened and (hopefully) what account was used to connect
     #>
 }
