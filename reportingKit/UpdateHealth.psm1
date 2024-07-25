0db679fd78c2dff846b3ccd7a3b7990cd4c28fcc
WickedIT <michael.gagnonf@gmail.com>
2024-07-24 20:21:57 -0700

    AudioDevSwap Update
    Changes to the swap mechanic and how the objects are handled.
    Added more verbose error handling.
    Added an opt-out for installing dependancy.

--- reportingKit/UpdateHealth.psm1
+++ reportingKit/UpdateHealth.psm1
@@ -1,11 +1,5 @@
 function Invoke-UpdateHealth {
     <#
-    Command: (Get-WsusServer).GetSubscription().GetLastSynchronizationInfo()
-        Calls the info from the last WSUS sync job -- not the currently running job.
-
-    collect sync info
-
-    collect status of computers with remaining critical patches and health of device
-    
+    Collect available updates from each Windows device to ensure nothing is hanging.
     #> 
     }
--- toolKit/AudioDevSwap.ps1
+++ toolKit/AudioDevSwap.ps1
@@ -1,75 +1,85 @@
 function Invoke-AudioDevSwap {
     $module = 'AudioDeviceCmdlets'
-    if (!(Get-Module -Name $module -ListAvailable)) {#Checks that module is installed, installs if not.
-        try {
-            Install-Module -name $module -Force -Verbose -ErrorAction Stop
+    if (!(Get-Module -Name $module -ListAvailable)) {#Checks that module is installed, requests permission and installs if not.
+        $continue = Read-Host "The module 'AudioDeviceCmdlet' is required but not installed, would you like to install it? (y/n)"
+        if ($continue.ToLower -eq 'y') {
+            try {
+                Install-Module -name $module -Force -Verbose -ErrorAction Stop
+            }
+            catch {
+                Write-Error "Unable to install the '$module' module from PSGallery."
+                return
+            }
         }
-        catch {
-            Write-Error "Unable to install the '$module' module from PSGallery."
-            break
-        }
+        else {exit}
     }
     if (!(Get-Module -name $module)) {#Checks that $module is loaded, loads and continues if not.
         try {
             Import-Module -name $module -Force -ErrorAction Stop
         }
         catch {
-            Write-Error "Unable to import module. Check that it installed correctly."
-            break
+            Write-Error "Unable to import $module module. Check that it installed correctly."
+            return
         }
     }
 
     if (Get-module -Name AudioDeviceCmdlets) {#Tests for AudioDeviceCmdlets.
-        if ( ! (Test-Path -Path .\swap_devices.txt)) {#Test for list of devices to swap to, creates and fills file if not.
+        if ( ! (Test-Path -Path $PSScriptRoot\swap_devices.txt)) {#Test for list of devices to swap to, creates and fills file if not.
             try {
-                $swapDevices = New-Item -Name swap_devices.txt -ErrorAction Stop
+                $swapDevices = New-Item -Path $PSScriptRoot\swap_devices.txt -ErrorAction Stop
             }
             catch {
-                Write-Error "Something went wrong : $swapDevices"
-                break
+                Write-Error "Something went wrong : Unable to create the text file for device list storage. Make sure the user running the script has permission to create files in the directory."
+                return
             }
             $devices= Get-AudioDevice -list | Select-Object -Property Name,ID
+            $n=0
             foreach ($device in $devices) {#Iterate through devices and print to host.
-                Write-Host "ID='$($device.ID)';Name='$($device.Name)'"
+                Write-Host "$n - '$($device.Name)'" -ForegroundColor Yellow -BackgroundColor Black
+                $n+=1
             }
-            Write-Host "Please input the 2 devices to switch between. (Hint copy/paste)"
-            $audioDevOne = Read-host "Audio Device One " | Out-File $swapDevices -Append
-            $audioDevTwo = Read-Host "Audio Device Two " | Out-File $swapDevices -Append
+            Write-Host "Please input the '#' placeholder for the 2 devices to switch between. " -ForegroundColor Black -BackgroundColor Yellow
+            $devOne=Read-Host "Audio Device #1 "
+            $devTwo=Read-Host "Audio Device #2 "
+            
+            @(
+                $devices[$devOne].ID
+                $devices[$devTwo].ID
+            ) | Set-Content -Path $swapDevices
         }
         else {#Sources swap_devices.txt for devices and loads to variables.
             try {
-                $swapDevices = Get-Item swap_devices.txt -ErrorAction Stop
+                $swapDevices = Get-ChildItem -Path $PSScriptRoot\swap_devices.txt -ErrorAction Stop
             }
             catch {
-                Write-Error "Something went wrong : $swapDevices"
-                break
+                Write-Error "Something went wrong : Unable to read the text file @ '$($swapDevices)'. Make sure that the script is in the same folder as the 'swapDevices.txt' file and the file contains the 2 ID's. If anything is wrong with the content, recreate the file."
+                return
             }
-            $audioDevOne = (Get-Content $swapDevices)[0]
-            $audioDevTwo = (Get-Content $swapDevices)[1]
         }
+        $audiodev = Get-Content -Path $swapDevices | Select-Object -First 2
         $currentPlayback = Get-AudioDevice -Playback
-        if ($currentPlayback.ID -eq $audioDevOne) {#Check if playback device is set to Dev#1 | switches to Dev#2
+        if ($currentPlayback.ID -eq $audioDev[0]) {#Check if playback device is set to Dev#1 | switches to Dev#2
             try{
-                $newPlayback= Set-AudioDevice -ID $audioDevTwo -ErrorAction Stop
+                $newPlayback= Set-AudioDevice -ID $audioDev[1] -ErrorAction Stop
                 $wshell= New-Object -ComObject Wscript.Shell
                 $wshell.Popup("Playback Device set to: $($newPlayback.Name)")
                 
             }
             catch {
                 Write-Error "Device not set. Current playback is still '$($currentPlayback.name)'"
-                break
+                return
             }
         }
         else{#If playback was Dev#2 | switches to Dev#1
             try{
-                $newPlayback = Set-AudioDevice -ID $audioDevOne -ErrorAction Stop
+                $newPlayback = Set-AudioDevice -ID $audioDev[0] -ErrorAction Stop
                 $wshell = New-Object -ComObject Wscript.Shell
                 $wshell.Popup("Playback Device set to: $($newPlayback.Name)")
                 
             }
             catch {
                 Write-Error "Device not set. Current playback is still '$($currentPlayback.name)'"
-                break
+                return
             }
         }
     }
