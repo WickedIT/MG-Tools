1fa7b2a7b3ddbf7c3ce9b2fc37a4e7d0ce823493
WickedIT <97221806+WickedIT@users.noreply.github.com>
2025-01-20 16:56:20 -0800

    Update nix_update.ps1

--- toolKit/nix_update.ps1
+++ toolKit/nix_update.ps1
@@ -1,10 +1,11 @@
 function Update-NixDevices {
     param([Parameter(Mandatory=$True)][System.IO.FileInfo]$path)
+    $date = Get-Date -Format "dd_MM_yyyy"
     $devices = Get-Content -path $path
     foreach ($pair in $devices) {
         Write-Host "Updating $pair..." -ForegroundColor Yellow -BackgroundColor Blue
-        $content = Invoke-Command -ScriptBlock {ssh "$pair" "sudo apt update -y && sudo apt upgrade -y"}
-        $content | Out-File -Path "Z:\for_Lab\script_Outputs\+nix_updates\$($pair.split('@')[1]).txt"
+        $content = Invoke-Command -ScriptBlock {ssh "$pair" "sudo apt update -y && sudo apt upgrade -y"} -ErrorAction SilentlyContinue | Out-Null
+        $content | Out-File -Path "Z:\for_Lab\script_Outputs\+nix_updates\$($date)-$($pair.split('@')[1]).txt"
     }
 }
 Update-NixDevices -Path "C:\ADMIN\The_Vault\+nix_devices.txt"
