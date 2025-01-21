7d22910ddd85e550407df335112244c24b0579d4
WickedIT <97221806+WickedIT@users.noreply.github.com>
2025-01-21 10:25:32 -0800

    Update test_port.psm1
    removed custom class because the runspaces just werent working properly.

--- reportingKit/test_port.psm1
+++ reportingKit/test_port.psm1
@@ -7,33 +7,17 @@ function Test-Ports {
     try {
         if ((Test-Connection -ComputerName $IP -Ping -Count 1).Status -eq 'Success') {
             $portcheck = New-Object System.Collections.ArrayList
-            1..65535 | Foreach-object -ThrottleLimit 500 -Parallel {
-                    #Defines class
-                    class Custom_Polling {
-                        [string]$Device
-                        [int]$Port
-                        [string]$Status
-                    
-                        Custom_Polling([string]$Device,[int]$port) {
-                            $this.Device = $Device
-                            $this.Port   = $port
-                            $this.Pulse()
-                        }
-                    
-                        [void] Pulse() {
-                            $this.Status = [Custom_Polling]::Test($this.Device,$this.Port)
-                        }
-                    
-                        [string] static Test([string]$Device,[int]$port) {
-                            $state = [Net.Sockets.TCPClient]::new().ConnectAsync($Device,$port).Wait(500)
-                            return $state
-                        }
-                    }
-                    #
-                    #
-                    #Calls and excutes class
+            1..65535 | Foreach-object -ThrottleLimit 500 -Parallel  {
+                    $device = $using:IP
                     try {
-                        $portcheck.Add([Custom_Polling]::new("$using:IP", $_))
+                        $scan = [Net.Sockets.TCPClient]::new().ConnectAsync($device,$_).Wait(500)
+                        if ($scan) {
+                            $status = [Ordered]@{
+                                Device = $device
+                                Status = $scan
+                            }
+                        }
+                        $portcheck.Add($status)
                         Write-Verbose "Scanning Port : $_"
                     }
                     catch{
